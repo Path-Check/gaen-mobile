eeccb04df03b7266d5bb8c878a0568d696b5c36f
Alejandro Dustet <aledustet@gmail.com>
2020-07-21 13:18:26 -0400

    Convert last batch of snapshot tests (#26)
    * Convert last batch of snapshot tests
    
    Why:
    ----
    Snapshot tests offer just one layer of testing, that the component is
    semantically correct but it is hard to understand if the change actually
    granted an update of the snapshot, making it not very valuable in
    confidence terms.
    
    This Commit:
    ----
    - Convert IconButton specs from snapshots
    - Remove unused Switch component
    - Remove home index snapshot spec in favor of home component spec
      * This one was removed because this component is acting as a proxy for
      the presentational one, that is tested.
    
    * Transform EulaModal to typescript
    
    - Inline checkbox and close icon only used on the EulaModal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,3 @@
 export { Button } from "./Button"
-export { Checkbox } from "./Checkbox"
-export { IconButton } from "./IconButton"
 export { NavigationBarWrapper } from "./NavigationBarWrapper"
-export { Switch } from "./Switch"
 export { RTLEnabledText } from "./RTLEnabledText"
