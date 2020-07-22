a8fd8f83fd0a7d932be0f73522bc4b002441d64d
Devin Jameson <devinjameson@me.com>
2020-07-22 13:22:18 -0400

    Clean up MainNavigator

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -22,32 +22,36 @@ const SCREEN_OPTIONS = {
 
 const MainNavigator: FunctionComponent = () => {
   const { isComplete } = useOnboardingContext()
+  const isOnboardingComplete = isComplete
   const displayNextSteps =
     env.DISPLAY_SELF_ASSESSMENT === "true" || env.AUTHORITY_ADVICE_URL
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={SCREEN_OPTIONS}>
-        {isComplete ? (
-          <Stack.Screen name={"App"} component={MainTabNavigator} />
+        {isOnboardingComplete ? (
+          <>
+            <Stack.Screen name={"App"} component={MainTabNavigator} />
+            <Stack.Screen
+              name={Stacks.AffectedUserStack}
+              component={AffectedUserStack}
+              options={{ ...TransitionPresets.ModalTransition }}
+            />
+            <Stack.Screen
+              name={Screens.MoreInfo}
+              component={MoreInfo}
+              options={{ ...TransitionPresets.ModalTransition }}
+            />
+            {displayNextSteps && (
+              <Stack.Screen
+                name={Screens.NextSteps}
+                component={NextSteps}
+                options={{ ...TransitionPresets.ModalTransition }}
+              />
+            )}
+          </>
         ) : (
           <Stack.Screen name={Stacks.Onboarding} component={OnboardingStack} />
-        )}
-        <Stack.Screen
-          name={Stacks.AffectedUserStack}
-          component={AffectedUserStack}
-          options={{ ...TransitionPresets.ModalTransition }}
-        />
-        <Stack.Screen
-          name={Screens.MoreInfo}
-          component={MoreInfo}
-          options={{ ...TransitionPresets.ModalTransition }}
-        />
-        {displayNextSteps && (
-          <Stack.Screen
-            name={Screens.NextSteps}
-            component={NextSteps}
-            options={{ ...TransitionPresets.ModalTransition }}
-          />
         )}
       </Stack.Navigator>
     </NavigationContainer>
