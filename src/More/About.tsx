68665d0ca132744ac5447b33f9831090cd5044da
Devin Jameson <devinjameson@me.com>
2020-07-29 18:58:53 -0400

    Clean up About and Licenses styling

--- src/More/About.tsx
+++ src/More/About.tsx
@@ -22,6 +22,9 @@ export const AboutScreen: FunctionComponent = () => {
   const { t } = useTranslation()
 
   const versionInfo = `${getVersion()} (${getBuildNumber()})`
+  const osInfo = `${Platform.OS} v${Platform.Version}`
+  const pathCheckWebAddress = "pathcheck.org"
+  const pathCheckUrl = "https://pathcheck.org/"
 
   return (
     <ScrollView
@@ -39,28 +42,26 @@ export const AboutScreen: FunctionComponent = () => {
       <RTLEnabledText
         style={style.hyperlink}
         onPress={() => {
-          Linking.openURL("https://pathcheck.org/")
+          Linking.openURL(pathCheckUrl)
         }}
       >
-        <Text>pathcheck.org</Text>
+        <Text>{pathCheckWebAddress}</Text>
       </RTLEnabledText>
-
-      <View style={style.rowContainer}>
-        <View style={style.row}>
+      <View style={style.infoRowContainer}>
+        <View style={style.infoRow}>
           <RTLEnabledText style={style.aboutSectionParaLabel}>
             {t("about.version")}
           </RTLEnabledText>
-
           <RTLEnabledText style={style.aboutSectionParaContent}>
             {versionInfo}
           </RTLEnabledText>
         </View>
-        <View style={style.row}>
+        <View style={style.infoRow}>
           <RTLEnabledText style={style.aboutSectionParaLabel}>
             {t("about.operating_system_abbr")}
           </RTLEnabledText>
           <RTLEnabledText style={style.aboutSectionParaContent}>
-            {Platform.OS + " v" + Platform.Version}
+            {osInfo}
           </RTLEnabledText>
         </View>
       </View>
@@ -70,16 +71,17 @@ export const AboutScreen: FunctionComponent = () => {
 
 const style = StyleSheet.create({
   contentContainer: {
-    backgroundColor: Colors.primaryBackground,
-    paddingHorizontal: Spacing.medium,
-    paddingTop: Spacing.huge,
     flex: 1,
+    backgroundColor: Colors.primaryBackgroundFaintShade,
+    paddingTop: Spacing.large,
+    paddingHorizontal: Spacing.small,
   },
   headerContent: {
-    ...Typography.header2,
+    ...Typography.header3,
     marginBottom: Spacing.small,
   },
   hyperlink: {
+    ...Typography.secondaryContent,
     color: Colors.linkText,
     textDecorationLine: "underline",
   },
@@ -88,17 +90,17 @@ const style = StyleSheet.create({
   },
   aboutSectionParaLabel: {
     ...Typography.header5,
-    width: Spacing.xxxHuge * 2,
+    width: 100,
     marginTop: Spacing.small,
   },
   aboutSectionParaContent: {
     ...Typography.mainContent,
     marginTop: Spacing.small,
   },
-  rowContainer: {
+  infoRowContainer: {
     marginTop: Spacing.medium,
   },
-  row: {
+  infoRow: {
     flexDirection: "row",
   },
 })
