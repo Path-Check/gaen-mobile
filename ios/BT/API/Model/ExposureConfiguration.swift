3151a419199c1aafab425d5ea36f1cd571d6a0d7
Matt Buckley <matt@nicethings.io>
2020-07-29 21:58:44 -0400

    Update exposure configuration.

--- ios/BT/API/Model/ExposureConfiguration.swift
+++ ios/BT/API/Model/ExposureConfiguration.swift
@@ -16,7 +16,7 @@ extension ExposureConfiguration {
 
   static var placeholder: ExposureConfiguration = {
     ExposureConfiguration(minimumRiskScore: 0,
-                          attenuationDurationThresholds: [50, 70],
+                          attenuationDurationThresholds: [53, 60],
                           attenuationLevelValues: [1, 2, 3, 4, 5, 6, 7, 8],
                           daysSinceLastExposureLevelValues: [1, 2, 3, 4, 5, 6, 7, 8],
                           durationLevelValues: [1, 2, 3, 4, 5, 6, 7, 8],
