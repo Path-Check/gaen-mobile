258cfaf85b86d1af1bb838886de832d8716023e4
Emiliano Galitiello <emilianocesar@gmail.com>
2020-08-03 16:14:17 -0300

    fix language list add safe area

--- src/More/LanguageSelection.tsx
+++ src/More/LanguageSelection.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import { FlatList, View, StyleSheet, TouchableHighlight } from "react-native"
 import { useTranslation } from "react-i18next"
 import { useNavigation } from "@react-navigation/native"
+import { useStatusBarEffect } from "../navigation"
 
 import { getLocaleList, setUserLocaleOverride } from "../locales/languages"
 import { GlobalText } from "../components/GlobalText"
 
 import { Colors, Spacing, Typography } from "../styles"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 const Separator = () => (
   <View
@@ -23,6 +25,7 @@ const LanguageSelection = (): JSX.Element => {
     i18n: { language },
   } = useTranslation()
   const navigation = useNavigation()
+  useStatusBarEffect("dark-content")
   const localeList = getLocaleList()
 
   const onSelectLanguage = (locale: string) => {
@@ -31,30 +34,32 @@ const LanguageSelection = (): JSX.Element => {
   }
 
   return (
-    <FlatList
-      keyExtractor={(_, i) => `${i}`}
-      data={localeList}
-      renderItem={({ item: { value, label } }) => (
-        <TouchableHighlight
-          underlayColor={Colors.underlayPrimaryBackground}
-          style={{
-            paddingVertical: Spacing.medium,
-            paddingHorizontal: Spacing.large,
-          }}
-          onPress={() => onSelectLanguage(value)}
-        >
-          <GlobalText
+    <SafeAreaView>
+      <FlatList
+        keyExtractor={(_, i) => `${i}`}
+        data={localeList}
+        renderItem={({ item: { value, label } }) => (
+          <TouchableHighlight
+            underlayColor={Colors.underlayPrimaryBackground}
             style={{
-              ...Typography.mainContent,
-              fontWeight: language === value ? "700" : "500",
+              paddingVertical: Spacing.medium,
+              paddingHorizontal: Spacing.large,
             }}
+            onPress={() => onSelectLanguage(value)}
           >
-            {label}
-          </GlobalText>
-        </TouchableHighlight>
-      )}
-      ItemSeparatorComponent={() => <Separator />}
-    />
+            <GlobalText
+              style={{
+                ...Typography.mainContent,
+                fontWeight: language === value ? "700" : "500",
+              }}
+            >
+              {label}
+            </GlobalText>
+          </TouchableHighlight>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+    </SafeAreaView>
   )
 }
 
