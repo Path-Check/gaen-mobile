6802efa809e7e0f8a3b0c1472272a098e5be4b06
Alejandro Dustet <aledustet@gmail.com>
2020-08-06 17:14:30 -0400

    Fix e2e tests
    Why:
    ----
    The onboarding test flow is not working and detox is not building.
    
    This Commit:
    ----
    - Remove translations wrapper around Onboarding test
    - Remove page objects from e2e tests
    - Fix the Onboarding e2e test to match the new onboarding flow
    - Refactor detox scripts on package.json
    
    Co-authored-by: Thomas Paresi <thomas.paresi@thoughtbot.com>

--- e2e/Onboarding.spec.js
+++ e2e/Onboarding.spec.js
@@ -1,57 +1,70 @@
-import { languages } from "./helpers/language"
-import EnableNotifications from "./pages/EnableNotifications.po.js"
-import EnableExposureNotifications from "./pages/EnableExposureNotifications.po.js"
-import Welcome from "./pages/Welcome.po.js"
-import PersonalPrivacy from "./pages/PersonalPrivacy.po.js"
-import NotificationDetails from "./pages/NotificationDetails.po.js"
-import ShareDiagnosis from "./pages/ShareDiagnosis.po.js"
-import SignEula from "./pages/SignEula.po.js"
-import Home from "./pages/Home.po.js"
-
-describe.each(languages)(
-  `Onboarding test suite in %s`,
-  (locale, languageStrings) => {
-    beforeAll(async () => {
-      await device.launchApp({
-        newInstance: true,
-        languageAndLocale: {
-          language: locale,
-          locale,
-        },
-        permissions: { notifications: "YES" },
-      })
+describe("Onboarding", () => {
+  beforeAll(async () => {
+    await device.launchApp({
+      newInstance: true,
+      permissions: { notifications: "YES" },
     })
+  })
 
-    describe("Onboarding", () => {
-      it("User navigates through the onboarding flow", async () => {
-        await Welcome.isOnScreen(languageStrings)
-        await Welcome.tapButton(languageStrings)
+  it("User navigates through the onboarding flow", async () => {
+    await expect(element(by.label("Welcome to "))).toBeVisible()
+    await element(by.label("Get Started")).tap()
 
-        await SignEula.sign(languageStrings)
-        await SignEula.tapButton(languageStrings)
+    await expect(
+      element(by.label("Together, we can contain the spread of COVID-19")),
+    ).toBeVisible()
+    await element(by.label("How does it work?")).tap()
 
-        await PersonalPrivacy.isOnScreen(languageStrings)
-        await PersonalPrivacy.tapButton(languageStrings)
+    await expect(
+      element(
+        by.label(
+          "Your phone remembers other devices it meets, but won't identify you to anyone",
+        ),
+      ),
+    ).toBeVisible()
+    await element(by.label("Is my privacy protected?")).tap()
 
-        await NotificationDetails.isOnScreen(languageStrings)
-        await NotificationDetails.tapButton(languageStrings)
+    await expect(
+      element(
+        by.label(
+          "No personally identifiable information ever leaves your phone",
+        ),
+      ),
+    ).toBeVisible()
+    await element(by.label("What if I'm exposed?")).tap()
 
-        await ShareDiagnosis.isOnScreen(languageStrings)
-        await ShareDiagnosis.tapButton()
+    await expect(
+      element(
+        by.label(
+          "The app will notify you if you may have been exposed to the coronavirus",
+        ),
+      ),
+    ).toBeVisible()
+    await element(by.label("What if I get sick?")).tap()
 
-        await EnableNotifications.isOnScreen(languageStrings)
-        await EnableNotifications.tapButton(languageStrings)
+    await expect(
+      element(
+        by.label(
+          "With the app, you can help break the chain of infection and get help direct from health experts",
+        ),
+      ),
+    ).toBeVisible()
+    await element(by.label("Get started")).tap()
 
-        await EnableExposureNotifications.isOnScreen(languageStrings)
-        await EnableExposureNotifications.tapButton(languageStrings)
+    await element(
+      by.label("I agree that I have read and accepted the terms of use"),
+    ).tap()
+    await element(by.label("Continue")).tap()
 
-        await Home.isOnScreen()
-      })
+    await element(by.label("Activate Proximity Tracing")).atIndex(1).tap()
+    await element(by.label("Enable Notifications")).tap()
+    await expect(
+      element(by.label("Exposure Notifications Disabled")),
+    ).toBeVisible()
+  })
 
-      afterAll(async () => {
-        await device.uninstallApp()
-        await device.installApp()
-      })
-    })
-  },
-)
+  afterAll(async () => {
+    await device.uninstallApp()
+    await device.installApp()
+  })
+})
