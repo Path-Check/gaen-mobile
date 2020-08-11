3c69d82da45efcd389171a8426a4dc033a97fb32
Alejandro Dustet <aledustet@gmail.com>
2020-08-11 17:22:01 -0400

    Fix exposure keys serialization (#178)
    * Change exposure key values order on serialization
    
    Why:
    ----
    The [specification] says the hmac calculation on exposure keys should have
    the base64 encoded key, then the rolling start, then the rolling period
    and finally the transmission risk joined by a `.`
    
    This Commit:
    ----
    - Change the order of the exposure key serialization to put the
    `rollingStartNumber` first and the `rollingPeriod` after
    
    [specification]: https://developers.google.com/android/exposure-notifications/verification-system#hmac-calc
    
    * Use default sorting and avoid re-converting keys
    
    Why:
    ----
    The sorting algorithm for the list of exposure keys used to calculate
    the hmac should be lexicographically, meaning, numbers, symbols,
    capitals and lowercase. Also the keys from the native side are sent in
    base64 encoding so there's no need to re-encode them
    
    This Commit:
    ----
    - Removes the encoding logic for the TEKeys
    - Sorts the list of exposure keys with the default sort algorithm

--- src/AffectedUserFlow/hmac.ts
+++ src/AffectedUserFlow/hmac.ts
@@ -2,11 +2,6 @@ import RNSimpleCrypto from "react-native-simple-crypto"
 
 import { ExposureKey } from "../exposureKey"
 
-const utf8ToBase64String = (input: string): string => {
-  const utf8AsArrayBuffer = RNSimpleCrypto.utils.convertUtf8ToArrayBuffer(input)
-  return RNSimpleCrypto.utils.convertArrayBufferToBase64(utf8AsArrayBuffer)
-}
-
 export const generateKey = async (): Promise<ArrayBuffer> => {
   return await RNSimpleCrypto.utils.randomBytes(32)
 }
@@ -34,43 +29,14 @@ export const calculateHmac = async (
 }
 
 const serializeKeys = (exposureKeys: ExposureKey[]) => {
-  const serializer = allRisksAreZero(exposureKeys)
-    ? serializeExposureKeyWithoutRisk
-    : serializeFullExposureKey
-  return exposureKeys
-    .map(serializer)
-    .sort((left: string, right: string) => {
-      return left.localeCompare(right, "en", { sensitivity: "base" })
-    })
-    .join(",")
+  return exposureKeys.map(serializeExposureKey).sort().join(",")
 }
 
-const serializeFullExposureKey = ({
+const serializeExposureKey = ({
   key,
-  rollingPeriod,
   rollingStartNumber,
+  rollingPeriod,
   transmissionRisk,
 }: ExposureKey): string => {
-  return [
-    utf8ToBase64String(key),
-    rollingPeriod,
-    rollingStartNumber,
-    transmissionRisk,
-  ].join(".")
-}
-
-const serializeExposureKeyWithoutRisk = ({
-  key,
-  rollingPeriod,
-  rollingStartNumber,
-}: ExposureKey): string => {
-  return [utf8ToBase64String(key), rollingPeriod, rollingStartNumber].join(".")
-}
-
-const allRisksAreZero = (exposureKeys: ExposureKey[]): boolean => {
-  return (
-    exposureKeys.filter(({ transmissionRisk }) => {
-      return transmissionRisk > 0
-    }).length === 0
-  )
+  return [key, rollingStartNumber, rollingPeriod, transmissionRisk].join(".")
 }
