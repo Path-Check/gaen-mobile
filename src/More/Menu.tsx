7746db5f34c3fc0a6d5dd9ff1038092a486e7d1f
Matt Buckley <matt@nicethings.io>
2020-08-24 14:15:16 -0400

    Show debug menu for dev and staging.

--- src/More/Menu.tsx
+++ src/More/Menu.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native"
 import { useTranslation } from "react-i18next"
 import { SvgXml } from "react-native-svg"
 import { useNavigation } from "@react-navigation/native"
+import env from "react-native-config"
 
 import { getLocalNames } from "../locales/languages"
 import { GlobalText } from "../components/GlobalText"
@@ -19,6 +20,7 @@ const MenuScreen: FunctionComponent = () => {
   } = useTranslation()
   const languageName = getLocalNames()[localeCode]
   useStatusBarEffect("light-content")
+  const showDebugMenu = env.DEV === "true" || env.STAGING === "true"
 
   const handleOnPressSelectLanguage = () => {
     navigation.navigate(Screens.LanguageSelection)
@@ -58,7 +60,7 @@ const MenuScreen: FunctionComponent = () => {
           lastItem
         />
       </View>
-      {__DEV__ ? (
+      {showDebugMenu ? (
         <View style={style.section}>
           <SettingsListItem
             label="EN Debug Menu"
