e01fac776561641ba111df919562a93e84841639
zfurjan <zeljko@rocketinsights.com>
2020-08-27 15:14:09 +0200

    Prettier fix

--- src/hooks/useConnectionStatus.ts
+++ src/hooks/useConnectionStatus.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import NetInfo from "@react-native-community/netinfo"
 
 export const useConnectionStatus = (): boolean | null | undefined => {
