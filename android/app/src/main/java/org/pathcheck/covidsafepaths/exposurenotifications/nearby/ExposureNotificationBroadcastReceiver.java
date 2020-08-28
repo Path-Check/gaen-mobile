486181323b7b754fc6e5d2d5ca089d926cf0d6aa
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2020-08-28 17:10:58 -0300

    Pull exposure keys every 12 hours (#291)
    * Pull exposure keys every 12 hours
    
    * Documentation

--- android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/nearby/ExposureNotificationBroadcastReceiver.java
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/nearby/ExposureNotificationBroadcastReceiver.java
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import com.google.android.gms.nearby.exposurenotification.ExposureNotificationClient;
+import org.pathcheck.covidsafepaths.exposurenotifications.ExposureNotificationClientWrapper;
 
 /**
  * Receiver registered for notifications of a potential exposure.
- * This receiver is called by the API after we pass them the keys: client.provideDiagnosisKeys()
- * Documentation: https://developers.google.com/android/exposure-notifications/exposure-notifications-api#broadcast-receivers
+ * This receiver is called by the API after we pass them the keys via
+ * {@link ExposureNotificationClientWrapper#provideDiagnosisKeys}
+ *
+ * @see <a href="Documentation">https://developers.google.com/android/exposure-notifications/exposure-notifications-api#broadcast-receivers</a>
  */
 public class ExposureNotificationBroadcastReceiver extends BroadcastReceiver {
 
