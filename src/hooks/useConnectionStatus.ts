eed6e6e66e95dbcd0e72dafd1258dec1ffd90f03
zfurjan <zeljko@rocketinsights.com>
2020-08-31 16:55:53 +0200

    Narow down types for connection state hook

--- src/hooks/useConnectionStatus.ts
+++ src/hooks/useConnectionStatus.ts
@@ -1,16 +1,14 @@
 import { useState, useEffect } from "react"
 import NetInfo from "@react-native-community/netinfo"
 
-export const useConnectionStatus = (): boolean | null | undefined => {
-  const [isConnected, setIsConnected] = useState<boolean | null | undefined>(
-    true,
-  )
+export const useConnectionStatus = (): boolean => {
+  const [isConnected, setIsConnected] = useState<boolean>(true)
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
       // netInfo state comes as null while unresolved so to avoid flicker we only set component state
       // if the netInfo state is resolved to boolean
-      if (state.isInternetReachable !== null) {
+      if (typeof state.isInternetReachable === "boolean") {
         setIsConnected(state.isInternetReachable)
       }
     })
