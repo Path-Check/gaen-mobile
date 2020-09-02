fec7efe45ead1e3977b33dbf685be15b124fb984
devinjameson <39350030+devinjameson@users.noreply.github.com>
2020-09-02 15:26:41 -0400

    Use wrench icon instead of fix text on home screen
    Why: using text doesn't work because fix is much longer than three characters in many languages.
    
    Co-Authored-By: Amanda Beiner <aebeiner@gmail.com>

--- src/Home/ActivationStatus.tsx
+++ src/Home/ActivationStatus.tsx
@@ -8,6 +8,15 @@ import { GlobalText } from "../components"
 import { Icons } from "../assets"
 import { Colors, Iconography, Outlines, Spacing, Typography } from "../styles"
 
+type Content = {
+  bodyText: string
+  leftIcon: string
+  leftIconFill: string
+  rightIcon: string
+  onPress: () => void
+  accessibilityLabel: string
+}
+
 interface ActivationStatusProps {
   headerText: string
   isActive: boolean
@@ -25,38 +34,50 @@ export const ActivationStatus: FunctionComponent<ActivationStatusProps> = ({
 }) => {
   const { t } = useTranslation()
 
-  const bodyText = isActive ? t("common.enabled") : t("common.disabled")
-  const icon = isActive ? Icons.CheckInCircle : Icons.XInCircle
-  const iconFill = isActive ? Colors.success100 : Colors.danger75
+  const activeContent: Content = {
+    bodyText: t("common.enabled"),
+    leftIcon: Icons.CheckInCircle,
+    leftIconFill: Colors.success100,
+    rightIcon: Icons.HomeInfo,
+    onPress: infoAction,
+    accessibilityLabel: t("home.get_more_info", { technology: headerText }),
+  }
+
+  const inactiveContent: Content = {
+    bodyText: t("common.disabled"),
+    leftIcon: Icons.XInCircle,
+    leftIconFill: Colors.danger75,
+    rightIcon: Icons.Wrench,
+    onPress: fixAction,
+    accessibilityLabel: t("home.fix", { technology: headerText }),
+  }
+
+  const content = isActive ? activeContent : inactiveContent
 
   return (
     <TouchableOpacity
-      onPress={isActive ? infoAction : fixAction}
+      onPress={content.onPress}
       style={style.activationStatusContainer}
+      accessible
+      accessibilityLabel={content.accessibilityLabel}
       testID={testID}
     >
       <View style={style.activationStatusLeftContainer}>
         <SvgXml
-          xml={icon}
-          fill={iconFill}
+          xml={content.leftIcon}
+          fill={content.leftIconFill}
           width={Iconography.medium}
           height={Iconography.medium}
         />
         <View style={style.activationStatusTextContainer}>
           <GlobalText style={style.bottomHeaderText}>{headerText}</GlobalText>
-          <GlobalText style={style.bottomBodyText}>{bodyText}</GlobalText>
+          <GlobalText style={style.bottomBodyText}>
+            {content.bodyText}
+          </GlobalText>
         </View>
       </View>
       <View style={style.activationStatusRightContainer}>
-        {isActive ? (
-          <SvgXml xml={Icons.HomeInfo} />
-        ) : (
-          <View style={style.fixButtonContainer}>
-            <GlobalText style={style.fixButtonText}>
-              {t("home.bluetooth.fix")}
-            </GlobalText>
-          </View>
-        )}
+        <SvgXml xml={content.rightIcon} />
       </View>
     </TouchableOpacity>
   )
@@ -83,18 +104,6 @@ const style = StyleSheet.create({
   activationStatusRightContainer: {
     width: rightColumnWidth,
     alignItems: "center",
-  },
-  fixButtonContainer: {
-    alignItems: "center",
-    backgroundColor: Colors.secondary50,
-    paddingVertical: Spacing.xxxSmall,
-    paddingHorizontal: Spacing.small,
-    borderRadius: Outlines.baseBorderRadius,
-  },
-  fixButtonText: {
-    ...Typography.header4,
-    color: Colors.primary100,
-    textTransform: "uppercase",
   },
   bottomHeaderText: {
     ...Typography.header4,
