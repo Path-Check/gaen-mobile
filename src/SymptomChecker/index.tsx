14a596a7bb0f536389542a3608a2bc3e371993d5
Devin Jameson <devinjameson@me.com>
2020-09-15 14:57:07 -0400

    Add base content to symptom checker

--- src/SymptomChecker/index.tsx
+++ src/SymptomChecker/index.tsx
@@ -1,8 +1,39 @@
 import React, { FunctionComponent } from "react"
-import { View } from "react-native"
+import { StyleSheet, ScrollView } from "react-native"
+import { useTranslation } from "react-i18next"
+
+import { useStatusBarEffect } from "../navigation"
+import { StatusBar, GlobalText } from "../components"
+
+import { Colors, Typography, Spacing } from "../styles"
 
 const SymptomCheckerScreen: FunctionComponent = () => {
-  return <View />
+  useStatusBarEffect("dark-content", Colors.primaryLightBackground)
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <StatusBar backgroundColor={Colors.primaryLightBackground} />
+      <ScrollView contentContainerStyle={style.contentContainer}>
+        <GlobalText style={style.headerText}>
+          {t("symptom_checker.my_health")}
+        </GlobalText>
+      </ScrollView>
+    </>
+  )
 }
+
+const style = StyleSheet.create({
+  contentContainer: {
+    flexGrow: 1,
+    backgroundColor: Colors.primaryLightBackground,
+    paddingTop: Spacing.xxxHuge,
+    paddingHorizontal: Spacing.large,
+  },
+  headerText: {
+    ...Typography.header1,
+    ...Typography.bold,
+  },
+})
 
 export default SymptomCheckerScreen
