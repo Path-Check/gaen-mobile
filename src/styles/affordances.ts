ed10f8f0e06fdb4861ca2e7147601602e7930e27
devinjameson <39350030+devinjameson@users.noreply.github.com>
2020-09-22 15:56:01 -0400

    Add react native flash message
    Why:
    ----
    
    When some of the operations that require some user input are completed,
    we should be showing some feedback to the users that the operation was
    successful.
    
    This Commit:
    ----
    - Add react native flash message library
    - Add styles to the flash message overlay
    - Show success message after symptoms are saved
    
    Co-authored-by: Alejandro Dustet <alejandro@thoughtbot.com>

--- src/styles/affordances.ts
+++ src/styles/affordances.ts
@@ -1,6 +1,8 @@
 import { ViewStyle } from "react-native"
+import { MessageOptions } from "react-native-flash-message"
 
 import * as Colors from "./colors"
+import * as Typography from "./typography"
 
 export const iconBadge: ViewStyle = {
   position: "absolute",
@@ -12,4 +14,21 @@ export const iconBadge: ViewStyle = {
   height: 12,
   justifyContent: "center",
   alignItems: "center",
+}
+
+type FlashMessageOptions = Omit<MessageOptions, "message">
+
+export const flashMessageOptions: FlashMessageOptions = {
+  floating: true,
+  titleStyle: { ...Typography.header3, color: Colors.black },
+}
+
+export const successFlashMessageOptions: FlashMessageOptions = {
+  ...flashMessageOptions,
+  backgroundColor: Colors.success100,
+}
+
+export const errorFlashMessageOptions: FlashMessageOptions = {
+  ...flashMessageOptions,
+  backgroundColor: Colors.danger75,
 }
