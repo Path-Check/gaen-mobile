0593fdee44aae3ab70973fca804cfa9f3f9186af
Matt Buckley <matt.buckley@rocketinsights.com>
2020-09-23 21:40:36 -0400

    Dismissible Symptom Input Form (#433)
    ### Why
    The designs (below) do not specify a way to close the symptom input form if the user decides that they do not wish to submit the form after opening the modal.
    <img width="334" alt="Screen Shot 2020-09-22 at 9 26 12 PM" src="https://user-images.githubusercontent.com/2637355/93954212-1e266b00-fd1b-11ea-9c57-734d2664e189.png">
    
    
    ### This Commit
    This commit adds a "dismiss" button to the symptom entry form:
    ![Sep-22-2020 21-26-49](https://user-images.githubusercontent.com/2637355/93954222-22eb1f00-fd1b-11ea-8175-8c4d61f84c23.gif)

--- src/MyHealth/SelectSymptoms.tsx
+++ src/MyHealth/SelectSymptoms.tsx
@@ -1,5 +1,11 @@
 import React, { FunctionComponent, useState } from "react"
-import { View, TouchableHighlight, StyleSheet, ScrollView } from "react-native"
+import {
+  View,
+  TouchableHighlight,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native"
 import { useTranslation } from "react-i18next"
 import { useNavigation } from "@react-navigation/native"
 
@@ -9,7 +15,16 @@ import { useMyHealthContext } from "./MyHealthContext"
 import { HealthAssessment, determineHealthAssessment } from "./symptoms"
 import { showMessage } from "react-native-flash-message"
 
-import { Affordances, Colors, Spacing, Typography, Outlines } from "../styles"
+import {
+  Affordances,
+  Colors,
+  Spacing,
+  Typography,
+  Outlines,
+  Iconography,
+} from "../styles"
+import { SvgXml } from "react-native-svg"
+import { Icons } from "../assets"
 
 const SelectSymptomsScreen: FunctionComponent = () => {
   useStatusBarEffect("dark-content", Colors.primaryLightBackground)
@@ -75,14 +90,27 @@ const SelectSymptomsScreen: FunctionComponent = () => {
   return (
     <>
       <StatusBar backgroundColor={Colors.primaryLightBackground} />
+      <View style={style.headerContainer}>
+        <GlobalText style={style.headerText}>
+          {t("symptom_checker.what_symptoms")}
+        </GlobalText>
+        <TouchableOpacity
+          style={style.closeIconContainer}
+          onPress={navigation.goBack}
+        >
+          <SvgXml
+            xml={Icons.XInCircle}
+            fill={Colors.neutral30}
+            width={Iconography.small}
+            height={Iconography.small}
+          />
+        </TouchableOpacity>
+      </View>
       <ScrollView
         style={style.container}
         contentContainerStyle={style.contentContainer}
         alwaysBounceVertical={false}
       >
-        <GlobalText style={style.headerText}>
-          {t("symptom_checker.what_symptoms")}
-        </GlobalText>
         <View style={style.symptomButtonsContainer}>
           {symptoms.map((value) => {
             return (
@@ -110,6 +138,8 @@ const SelectSymptomsScreen: FunctionComponent = () => {
   )
 }
 
+const headerHeight = 100
+
 const style = StyleSheet.create({
   container: {
     backgroundColor: Colors.primaryLightBackground,
@@ -121,9 +151,24 @@ const style = StyleSheet.create({
     paddingHorizontal: Spacing.large,
     paddingBottom: Spacing.xxHuge,
   },
+  closeIconContainer: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    padding: Spacing.medium,
+  },
+  headerContainer: {
+    width: "100%",
+    height: headerHeight,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+    backgroundColor: Colors.white,
+  },
   headerText: {
     ...Typography.header2,
     marginBottom: Spacing.large,
+    marginLeft: Spacing.small,
   },
   symptomButtonsContainer: {
     flexWrap: "wrap",
