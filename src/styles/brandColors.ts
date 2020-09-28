0d83560784fb7984027dd9490064d3064c466aca
John Schoeman <johnschoeman1617@gmail.com>
2020-09-28 14:40:37 -0400

    wip

---
+++ src/styles/brandColors.ts
@@ -0,0 +1,17 @@
+// Primary
+export const primary100 = "#4051db"
+export const primary110 = "#4754c5"
+export const primary125 = "#2434b6"
+export const primary150 = "#192591"
+
+// Secondary
+export const secondary10 = "#f8f8ff"
+export const secondary50 = "#e5e7fa"
+export const secondary75 = "#d3d7f8"
+export const secondary100 = "#a5affb"
+
+// Gradients
+export const gradientPrimary20Darker = "#f4edfe"
+export const gradientPrimary20Lighter = secondary10
+export const gradientPrimary100Darker = primary100
+export const gradientPrimary100Lighter = "#6979f8"
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,3 +1,5 @@
+import * as BrandColors from "./brandColors"
+
 const applyOpacity = (hexColor: string, opacity: number): string => {
   const red = parseInt(hexColor.slice(1, 3), 16)
   const green = parseInt(hexColor.slice(3, 5), 16)
@@ -19,16 +21,16 @@ export const neutral140 = "#1c2537"
 export const black = "#000000"
 
 // Primary
-export const primary100 = "#4051db"
-export const primary110 = "#4754c5"
-export const primary125 = "#2434b6"
-export const primary150 = "#192591"
+export const primary100 = BrandColors.primary100
+export const primary110 = BrandColors.primary110
+export const primary125 = BrandColors.primary125
+export const primary150 = BrandColors.primary150
 
 // Secondary
-export const secondary10 = "#f8f8ff"
-export const secondary50 = "#e5e7fa"
-export const secondary75 = "#d3d7f8"
-export const secondary100 = "#a5affb"
+export const secondary10 = BrandColors.secondary10
+export const secondary50 = BrandColors.secondary50
+export const secondary75 = BrandColors.secondary75
+export const secondary100 = BrandColors.secondary100
 
 // Accents
 export const danger75 = "#ff7d7d"
@@ -38,22 +40,36 @@ export const warning25 = "#f9edcc"
 export const warning50 = "#ffdc6f"
 export const warning100 = "#ffc000"
 
+// Backgrounds
+export const primaryLightBackground = white
+export const primaryDarkBackround = primary125
+
 // Gradients
-export const gradientPrimary10 = ["#ececff", "#ffffff"]
-export const gradientPrimary20 = ["#f4edfe", secondary10]
-export const gradientPrimary20Lighter = secondary10
-export const gradientPrimary100 = [primary125, "#6979f8"]
-export const gradientPrimary100Lighter = "#6979f8"
-export const gradientPrimary110 = [primary100, "#6e50e4"]
+export const gradientPrimary20Darker = BrandColors.gradientPrimary20Darker
+export const gradientPrimary20Lighter = BrandColors.gradientPrimary20Lighter
+export const gradientPrimary100Darker = BrandColors.gradientPrimary100Darker
+export const gradientPrimary100Lighter = BrandColors.gradientPrimary100Lighter
+
+export const gradientPrimary20 = [
+  gradientPrimary20Darker,
+  gradientPrimary20Lighter,
+]
+export const gradientPrimary100 = [
+  gradientPrimary100Darker,
+  gradientPrimary100Lighter,
+]
+
 export const gradientNeutral75 = [neutral75, neutral100]
+export const gradientPrimary10Darker = "#ececff"
+export const gradientPrimary10Lighter = primaryLightBackground
+export const gradientPrimary10 = [
+  gradientPrimary10Darker,
+  gradientPrimary10Lighter,
+]
 
 // Transparent
 export const transparent = "rgba(0, 0, 0, 0)"
 export const transparentNeutral30 = applyOpacity(neutral30, 0.4)
-
-// Backgrounds
-export const primaryLightBackground = white
-export const primaryDarkBackround = primary125
 
 // Headers
 export const headerBackground = primary125
