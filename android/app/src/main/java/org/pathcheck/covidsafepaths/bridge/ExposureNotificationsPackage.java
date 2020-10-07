08205e177bfd054cfaf3267d5e5663012632a6d8
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2020-10-07 15:57:13 -0300

    (Android) Remove CheckIn model from Realm

--- android/app/src/main/java/org/pathcheck/covidsafepaths/bridge/ExposureNotificationsPackage.java
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/bridge/ExposureNotificationsPackage.java
@@ -8,7 +8,6 @@ import com.facebook.react.uimanager.ViewManager;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.pathcheck.covidsafepaths.exposurenotifications.reactmodules.CheckInModule;
 import org.pathcheck.covidsafepaths.exposurenotifications.reactmodules.DebugMenuModule;
 import org.pathcheck.covidsafepaths.exposurenotifications.reactmodules.DeviceInfoModule;
 import org.pathcheck.covidsafepaths.exposurenotifications.reactmodules.ExposureHistoryModule;
@@ -28,7 +27,6 @@ public class ExposureNotificationsPackage implements ReactPackage {
     modules.add(new DeviceInfoModule(reactContext));
     modules.add(new ExposureHistoryModule(reactContext));
     modules.add(new UtilsModule(reactContext));
-    modules.add(new CheckInModule(reactContext));
     modules.add(new SymptomLogEntryModule(reactContext));
 
     return modules;
