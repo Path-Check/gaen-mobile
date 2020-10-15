37984a50eaec9efb30e4dee02a6a4abfe04b3c4f
Matt Buckley <matt.buckley@rocketinsights.com>
2020-10-15 10:09:22 -0400

    README Updates + Remove Detox Testing Infrastructure (#559)
    #### Why:
    We'd like the README to be up-to-date with the current state of the codebase
    
    
    #### This commit:
    This commit removes references to detox/e2e testing in from the `README`, and also removes unused e2e testing infrastructure that had been carried over from the `GPS` app

--- e2e/helpers/language.ts
+++
@@ -1,9 +0,0 @@
-import * as english from "../../src/locales/en.json"
-
-const languageStrings = {
-  "en-US": english,
-}
-
-export const languages = Object.entries(
-  languageStrings,
-).map(([locale, strings]) => [locale, { ...english, ...strings }])
