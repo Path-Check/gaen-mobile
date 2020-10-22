033d2a378dd00e103100f3a5e5509eac32101b4e
Matt Buckley <matt@nicethings.io>
2020-10-21 21:47:12 -0400

    Update delete data confirmations screen copy and navigation.

--- src/navigation/SettingsStack.tsx
+++ src/navigation/SettingsStack.tsx
@@ -2,10 +2,7 @@ import React, { FunctionComponent } from "react"
 import {
   createStackNavigator,
   StackNavigationOptions,
-  HeaderBackButton,
 } from "@react-navigation/stack"
-import { useTranslation } from "react-i18next"
-import { useNavigation } from "@react-navigation/native"
 
 import Settings from "../Settings/"
 import Legal from "../Settings/Legal"
@@ -14,49 +11,17 @@ import ENLocalDiagnosisKey from "../Settings/ENLocalDiagnosisKeyScreen"
 import ExposureListDebug from "../Settings/ExposureListDebugScreen"
 import { SettingsStackScreens } from "./index"
 
-import { Colors, Headers } from "../styles"
+import { Headers } from "../styles"
 import DeleteConfirmation from "../Settings/DeleteConfirmation"
+import { applyHeaderLeftBackButton } from "./HeaderLeftBackButton"
 
 const Stack = createStackNavigator()
 
-const headerLeftDark = () => {
-  return <HeaderLeft tintColor={Colors.white} />
-}
-
-const headerLeftLight = () => {
-  return <HeaderLeft tintColor={Colors.primaryDarkBackround} />
-}
-
-interface HeaderLeftProps {
-  tintColor: string
-}
-
-const HeaderLeft: FunctionComponent<HeaderLeftProps> = ({ tintColor }) => {
-  const navigation = useNavigation()
-
-  return (
-    <HeaderBackButton
-      labelVisible={false}
-      tintColor={tintColor}
-      onPress={() => navigation.goBack()}
-    />
-  )
-}
-
 const SettingsStack: FunctionComponent = () => {
-  const { t } = useTranslation()
-
   const defaultScreenOptions: StackNavigationOptions = {
-    headerStyle: {
-      ...Headers.headerStyle,
-    },
-    headerTitleStyle: {
-      ...Headers.headerTitleStyle,
-    },
-    headerBackTitleVisible: false,
-    headerTintColor: Colors.headerText,
-    headerTitleAlign: "center",
-    headerLeft: headerLeftDark,
+    ...Headers.headerMinimalOptions,
+    headerLeft: applyHeaderLeftBackButton(),
+    headerRight: () => null,
   }
 
   return (
@@ -69,30 +34,27 @@ const SettingsStack: FunctionComponent = () => {
       <Stack.Screen
         name={SettingsStackScreens.Legal}
         component={Legal}
-        options={{ headerTitle: t("screen_titles.legal") }}
+        options={defaultScreenOptions}
       />
       <Stack.Screen
         name={SettingsStackScreens.DeleteConfirmation}
         component={DeleteConfirmation}
-        options={{
-          headerTitle: "",
-          headerLeft: headerLeftLight,
-          headerStyle: { backgroundColor: Colors.primaryLightBackground },
-        }}
+        options={defaultScreenOptions}
       />
       <Stack.Screen
         name={SettingsStackScreens.ENDebugMenu}
         component={ENDebugMenu}
-        options={{ headerTitle: t("screen_titles.debug") }}
+        options={defaultScreenOptions}
       />
       <Stack.Screen
         name={SettingsStackScreens.ExposureListDebugScreen}
         component={ExposureListDebug}
-        options={{ headerTitle: t("screen_titles.exposures") }}
+        options={defaultScreenOptions}
       />
       <Stack.Screen
         name={SettingsStackScreens.ENLocalDiagnosisKey}
         component={ENLocalDiagnosisKey}
+        options={defaultScreenOptions}
       />
     </Stack.Navigator>
   )
