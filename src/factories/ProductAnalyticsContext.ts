7368f1194c050ef2d7c6c27c63bdc4a13b6cb62f
Matt Buckley <matt@nicethings.io>
2020-11-04 12:31:08 -0500

    wip

--- src/factories/ProductAnalyticsContext.ts
+++
@@ -1,9 +0,0 @@
-import { Factory } from "fishery"
-import { ProductAnalyticsContextState } from "../ProductAnalytics/Context"
-
-export default Factory.define<ProductAnalyticsContextState>(() => ({
-  userConsentedToAnalytics: false,
-  updateUserConsent: () => Promise.resolve(),
-  trackEvent: () => Promise.resolve(),
-  trackScreenView: () => Promise.resolve(),
-}))
