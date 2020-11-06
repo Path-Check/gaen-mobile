b2e821a671aefa83f23d9a2673a453e9a7c98cdf
artgibson <332115+artgibson@users.noreply.github.com>
2020-11-06 10:47:58 -0500

    fix About text sentence structure (#636)
    fix About text sentence structure
    
    Co-authored-by: Art Gibson <artgibson@users.noreply.github.com>

--- src/Settings/index.spec.tsx
+++ src/Settings/index.spec.tsx
@@ -85,7 +85,9 @@ describe("Settings", () => {
     )
 
     expect(
-      getByText(/The applicationName app is made available by authorityName/),
+      getByText(
+        /The applicationName app is made available by the authorityName/,
+      ),
     ).toBeDefined()
   })
 
