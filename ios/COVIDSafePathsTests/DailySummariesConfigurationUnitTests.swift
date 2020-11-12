c65e9150ff69eddc2342628d7d5460625ef62824
Matt Buckley <matt@nicethings.io>
2020-11-12 13:03:17 -0500

    wip

--- ios/COVIDSafePathsTests/DailySummariesConfigurationUnitTests.swift
+++ ios/COVIDSafePathsTests/DailySummariesConfigurationUnitTests.swift
@@ -7,15 +7,16 @@ import XCTest
 
 @testable import BT
 
-class ExposureConfigurationUnitTests: XCTestCase {
+class DailySummariesConfigurationUnitTests: XCTestCase {
 
+  // JSON can be deserialized into a DailySummariesConfiguration
   @available(iOS 13.7, *)
   func testValidDailySummariesConfiguration() {
     let dict: [String: Any] = ["DailySummariesConfig": ["attenuationDurationThresholds": [40,53,60],
                                                         "attenuationBucketWeights": [1,1,0.5,0],
-                                                        "reportTypeWeights": [1,0,0,0],
+                                                        "reportTypeWeights": [1,0,0,0] as [Double],
                                                         "reportTypeWhenMissing": 1,
-                                                        "infectiousnessWeights": [1,1],
+                                                        "infectiousnessWeights": [1,1] as [Double],
                                                         "infectiousnessWhenDaysSinceOnsetMissing": 1,
                                                         "daysSinceOnsetToInfectiousness": [[-2,1],
                                                                                            [-1,1],
@@ -37,8 +38,21 @@ class ExposureConfigurationUnitTests: XCTestCase {
                                "triggerThresholdWeightedDuration": 12]
     let jsonData = try! JSONSerialization.data(withJSONObject: dict)
     let config = DailySummariesConfiguration.create(from: jsonData)!
-    XCTAssertEqual(config.daysSinceOnsetToInfectiousness[NSNumber(value: ENDaysSinceOnsetOfSymptomsUnknown)], 1)
-    XCTAssertEqual(config.triggerThresholdWeightedDuration, 12)
+    let dictConfig = dict["DailySummariesConfig"] as! [String: Any]
+    let dictDaysSinceOnsetToInfectiousness: [NSNumber: NSNumber] = (dictConfig["daysSinceOnsetToInfectiousness"] as! [[NSNumber]]).reduce([NSNumber(value: ENDaysSinceOnsetOfSymptomsUnknown): 1], {(result, next) in
+      var copy = result
+      copy[next[0]] = next[1]
+      return copy
+    })
+
+    XCTAssertEqual(config.triggerThresholdWeightedDuration, dict["triggerThresholdWeightedDuration"] as! Int)
+    XCTAssertEqual(config.attenuationDurationThresholds, dictConfig["attenuationDurationThresholds"] as! [NSNumber])
+    XCTAssertEqual(config.attenuationBucketWeights, dictConfig["attenuationBucketWeights"] as! [Double])
+    XCTAssertEqual(config.reportTypeWeights, dictConfig["reportTypeWeights"] as! [Double])
+    XCTAssertEqual(config.reportTypeWhenMissing, dictConfig["reportTypeWhenMissing"] as! Int)
+    XCTAssertEqual(config.infectiousnessWeights, dictConfig["infectiousnessWeights"] as! [Double])
+    XCTAssertEqual(config.infectiousnessWhenDaysSinceOnsetMissing, dictConfig["infectiousnessWhenDaysSinceOnsetMissing"] as! Int)
+    XCTAssertEqual(config.daysSinceOnsetToInfectiousness, dictDaysSinceOnsetToInfectiousness)
   }
 
 }
