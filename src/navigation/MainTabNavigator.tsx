fb42ea0a20d6c40685045bb2e92771a1d632c03d
Devin Jameson <devinjameson@me.com>
2020-11-13 15:05:37 -0500

    WIP

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -13,176 +13,198 @@ import HomeStack from "./HomeStack"
 import SymptomHistoryStack from "./SymptomHistoryStack"
 import SettingsStack from "./SettingsStack"
 import { useConfigurationContext } from "../ConfigurationContext"
-import { Stacks } from "./index"
+import { TabRoute, TabRoutes } from "./index"
 import { TabBarIcons } from "../assets/svgs/TabBarNav"
 import { Text } from "../components"
 
 import { Colors, Layout, Outlines, Spacing, Typography } from "../styles"
 
+type Tab = {
+  name: TabRoute
+  component: FunctionComponent
+}
+
 const MainTabNavigator: FunctionComponent = () => {
-  const { t } = useTranslation()
-  const insets = useSafeAreaInsets()
   const { displaySymptomHistory } = useConfigurationContext()
 
-  interface TabBarIconProps {
-    focused: boolean
+  const homeTab = {
+    name: TabRoutes.Home,
+    component: HomeStack,
+  }
+  const exposureHistoryTab = {
+    name: TabRoutes.ExposureHistory,
+    component: ExposureHistoryStack,
+  }
+  const symptomHistoryTab = {
+    name: TabRoutes.SymptomHistory,
+    component: SymptomHistoryStack,
+  }
+  const settingsTab = {
+    name: TabRoutes.Settings,
+    component: SettingsStack,
   }
 
-  const HomeIcon: FunctionComponent<TabBarIconProps> = ({ focused }) => {
-    return <TabIcon icon={TabBarIcons.House} focused={focused} />
-  }
+  const tabs: Tab[] = displaySymptomHistory
+    ? [homeTab, exposureHistoryTab, symptomHistoryTab, settingsTab]
+    : [homeTab, exposureHistoryTab, settingsTab]
 
-  const ExposureHistoryIcon: FunctionComponent<TabBarIconProps> = ({
-    focused,
-  }) => {
-    const tabIcon = <TabIcon icon={TabBarIcons.Exposure} focused={focused} />
-    return tabIcon
-  }
+  const TabNavigator = createBottomTabNavigator()
 
-  const HeartbeatIcon: FunctionComponent<TabBarIconProps> = ({ focused }) => {
-    const tabIcon = <TabIcon icon={TabBarIcons.Heartbeat} focused={focused} />
-    return tabIcon
-  }
+  return (
+    <TabNavigator.Navigator
+      tabBar={(props) => <TabBar {...props} tabs={tabs} />}
+    >
+      {tabs.map(({ name, component }) => {
+        return (
+          <TabNavigator.Screen name={name} component={component} key={name} />
+        )
+      })}
+    </TabNavigator.Navigator>
+  )
+}
 
-  const SettingsIcon: FunctionComponent<TabBarIconProps> = ({ focused }) => {
-    const tabIcon = <TabIcon icon={TabBarIcons.Gear} focused={focused} />
-    return tabIcon
-  }
+type TabBarProps = BottomTabBarProps & { tabs: Tab[] }
 
-  interface TabIconProps extends TabBarIconProps {
-    icon: string
-  }
+const TabBar: FunctionComponent<TabBarProps> = ({
+  state,
+  navigation,
+  tabs,
+}) => {
+  const { t } = useTranslation()
+  const insets = useSafeAreaInsets()
 
-  const TabIcon: FunctionComponent<TabIconProps> = ({ focused, icon }) => {
-    const iconSize = 22
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingBottom: insets.bottom + Spacing.xxSmall,
+        paddingTop: Spacing.xxSmall,
+        backgroundColor: Colors.background.primaryLight,
+        borderTopWidth: Outlines.hairline,
+        borderColor: Colors.neutral.shade10,
+      }}
+    >
+      {state.routes.map((route, index: number) => {
+        const handleOnPress = () => {
+          !isFocused && navigation.navigate(route.name)
+        }
 
-    return (
+        const isFocused = state.index === index
+
+        const textColor = isFocused
+          ? Colors.primary.shade100
+          : Colors.neutral.shade100
+
+        const routeStringToTab = (route: string): TabRoute => {
+          switch (route) {
+            case "Home": {
+              return "Home"
+            }
+            case "ExposureHistory": {
+              return "ExposureHistory"
+            }
+            case "SymptomHistory": {
+              return "SymptomHistory"
+            }
+            case "Settings": {
+              return "Settings"
+            }
+            default: {
+              return "Home"
+            }
+          }
+        }
+
+        const currentTab = routeStringToTab(route.name)
+
+        type TabButtonConfig = {
+          label: string
+          icon: string
+        }
+
+        const determineConfig = (): TabButtonConfig => {
+          switch (currentTab) {
+            case "Home": {
+              return {
+                label: t("navigation.home"),
+                icon: TabBarIcons.House,
+              }
+            }
+            case "ExposureHistory": {
+              return {
+                label: t("navigation.exposure_history"),
+                icon: TabBarIcons.Exposure,
+              }
+            }
+            case "SymptomHistory": {
+              return {
+                label: t("navigation.symptom_history"),
+                icon: TabBarIcons.Heartbeat,
+              }
+            }
+            case "Settings": {
+              return {
+                label: t("navigation.settings"),
+                icon: TabBarIcons.Gear,
+              }
+            }
+          }
+        }
+
+        const { label, icon } = determineConfig()
+
+        return (
+          <Pressable
+            onPress={handleOnPress}
+            style={{
+              alignItems: "center",
+              width: Layout.screenWidth / tabs.length,
+            }}
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            key={index}
+          >
+            <TabIcon focused={isFocused} icon={icon} />
+            <Text
+              allowFontScaling={false}
+              numberOfLines={2}
+              ellipsizeMode="middle"
+              style={{
+                ...Typography.style.normal,
+                fontSize: Typography.size.x15,
+                color: textColor,
+                textAlign: "center",
+                lineHeight: Typography.lineHeight.x5,
+                maxWidth: Spacing.xxxMassive,
+              }}
+            >
+              {label}
+            </Text>
+          </Pressable>
+        )
+      })}
+    </View>
+  )
+}
+
+interface TabIconProps {
+  focused: boolean
+  icon: string
+}
+
+const TabIcon: FunctionComponent<TabIconProps> = ({ focused, icon }) => {
+  const iconSize = 22
+
+  return (
+    <View style={{ marginBottom: Spacing.xxSmall }}>
       <SvgXml
         xml={icon}
         fill={focused ? Colors.primary.shade100 : Colors.neutral.shade50}
         width={iconSize}
         height={iconSize}
       />
-    )
-  }
-
-  type Tab_ = "home" | "exposureHistory" | "settings" | "symptomHistory"
-  const tabs: Tab_[] = ["home", "exposureHistory", "settings"]
-  if (displaySymptomHistory) {
-    tabs.push("symptomHistory")
-  }
-
-  const TabBar: FunctionComponent<BottomTabBarProps> = ({
-    state,
-    descriptors,
-    navigation,
-  }) => {
-    return (
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          paddingBottom: insets.bottom + Spacing.xxSmall,
-          paddingTop: Spacing.xxSmall,
-          backgroundColor: Colors.background.primaryLight,
-          borderTopWidth: Outlines.hairline,
-          borderColor: Colors.neutral.shade10,
-        }}
-      >
-        {state.routes.map((route, index: number) => {
-          const { options } = descriptors[route.key]
-
-          const label = options.title
-          const icon = options.tabBarIcon
-          const isFocused = state.index === index
-
-          const handleOnPress = () => {
-            !isFocused && navigation.navigate(route.name)
-          }
-
-          const textColor = isFocused
-            ? Colors.primary.shade100
-            : Colors.neutral.shade100
-
-          if (icon === undefined) {
-            return
-          }
-
-          return (
-            <Pressable
-              onPress={handleOnPress}
-              style={{
-                alignItems: "center",
-                width: Layout.screenWidth / tabs.length,
-              }}
-              accessibilityRole="button"
-              accessibilityState={isFocused ? { selected: true } : {}}
-              key={index}
-            >
-              <View style={{ marginBottom: Spacing.xxSmall }}>
-                {icon({ focused: isFocused, color: "", size: 0 })}
-              </View>
-              <Text
-                allowFontScaling={false}
-                numberOfLines={2}
-                ellipsizeMode="middle"
-                style={{
-                  ...Typography.style.normal,
-                  fontSize: Typography.size.x15,
-                  color: textColor,
-                  textAlign: "center",
-                  lineHeight: Typography.lineHeight.x5,
-                  maxWidth: Spacing.xxxMassive,
-                }}
-              >
-                {label}
-              </Text>
-            </Pressable>
-          )
-        })}
-      </View>
-    )
-  }
-
-  const Tab = createBottomTabNavigator()
-
-  return (
-    <Tab.Navigator tabBar={TabBar}>
-      <Tab.Screen
-        name={Stacks.Home}
-        component={HomeStack}
-        options={{
-          title: t("navigation.home"),
-          tabBarIcon: HomeIcon,
-        }}
-      />
-      <Tab.Screen
-        name={Stacks.ExposureHistoryFlow}
-        component={ExposureHistoryStack}
-        options={{
-          title: t("navigation.exposure_history"),
-          tabBarIcon: ExposureHistoryIcon,
-        }}
-      />
-      {displaySymptomHistory && (
-        <Tab.Screen
-          name={Stacks.SymptomHistory}
-          component={SymptomHistoryStack}
-          options={{
-            title: t("navigation.symptom_history"),
-            tabBarIcon: HeartbeatIcon,
-          }}
-        />
-      )}
-      <Tab.Screen
-        name={Stacks.Settings}
-        component={SettingsStack}
-        options={{
-          title: t("navigation.settings"),
-          tabBarIcon: SettingsIcon,
-        }}
-      />
-    </Tab.Navigator>
+    </View>
   )
 }
 
