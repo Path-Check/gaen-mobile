baeff206a884f1dd3feedd11b010f753d818901a
Devin Jameson <devinjameson@me.com>
2020-11-17 16:16:54 -0500

    Update delete my data screen

--- src/Settings/DeleteConfirmation.spec.tsx
+++ src/Settings/DeleteConfirmation.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { fireEvent, render, waitFor } from "@testing-library/react-native"
-import { showMessage } from "react-native-flash-message"
+import { Alert } from "react-native"
+import { fireEvent, render } from "@testing-library/react-native"
 
 import { SymptomHistoryContext } from "../SymptomHistory/SymptomHistoryContext"
 import { OnboardingProvider } from "../OnboardingContext"
@@ -11,15 +11,14 @@ jest.mock("react-native-flash-message")
 jest.mock("@react-navigation/native")
 
 describe("DeleteConfirmation", () => {
-  it("allows users to delete their data", async () => {
-    const deleteAllEntriesSpy = jest.fn()
-    deleteAllEntriesSpy.mockResolvedValueOnce({ kind: "success" })
+  it("allows a user to delete their data", () => {
+    const alertSpy = jest.spyOn(Alert, "alert")
 
     const { getByLabelText } = render(
       <OnboardingProvider userHasCompletedOnboarding={false}>
         <SymptomHistoryContext.Provider
           value={factories.symptomHistoryContext.build({
-            deleteAllEntries: deleteAllEntriesSpy,
+            deleteAllEntries: jest.fn(),
           })}
         >
           <DeleteConfirmation />
@@ -28,66 +27,6 @@ describe("DeleteConfirmation", () => {
     )
 
     fireEvent.press(getByLabelText("Delete my data"))
-    await waitFor(() => {
-      expect(deleteAllEntriesSpy).toHaveBeenCalled()
-    })
-  })
-
-  describe("when data deletion is successful", () => {
-    it("presents a success message", async () => {
-      const showMessageSpy = showMessage as jest.Mock
-      const deleteAllEntriesSpy = jest.fn()
-      deleteAllEntriesSpy.mockResolvedValueOnce({ kind: "success" })
-
-      const { getByLabelText } = render(
-        <OnboardingProvider userHasCompletedOnboarding={false}>
-          <SymptomHistoryContext.Provider
-            value={factories.symptomHistoryContext.build({
-              deleteAllEntries: deleteAllEntriesSpy,
-            })}
-          >
-            <DeleteConfirmation />
-          </SymptomHistoryContext.Provider>
-        </OnboardingProvider>,
-      )
-
-      fireEvent.press(getByLabelText("Delete my data"))
-      await waitFor(() => {
-        expect(showMessageSpy).toHaveBeenCalledWith(
-          expect.objectContaining({
-            message: "Data deleted",
-          }),
-        )
-      })
-    })
-  })
-
-  describe("when data deletion fails", () => {
-    it("presents an error message", async () => {
-      const showMessageSpy = showMessage as jest.Mock
-      const deleteAllEntriesSpy = jest.fn()
-      deleteAllEntriesSpy.mockResolvedValueOnce({ kind: "failure" })
-
-      const { getByLabelText } = render(
-        <OnboardingProvider userHasCompletedOnboarding={false}>
-          <SymptomHistoryContext.Provider
-            value={factories.symptomHistoryContext.build({
-              deleteAllEntries: deleteAllEntriesSpy,
-            })}
-          >
-            <DeleteConfirmation />
-          </SymptomHistoryContext.Provider>
-        </OnboardingProvider>,
-      )
-
-      fireEvent.press(getByLabelText("Delete my data"))
-      await waitFor(() => {
-        expect(showMessageSpy).toHaveBeenCalledWith(
-          expect.objectContaining({
-            message: "Sorry, we could not delete your data",
-          }),
-        )
-      })
-    })
+    expect(alertSpy).toHaveBeenCalled()
   })
 })
