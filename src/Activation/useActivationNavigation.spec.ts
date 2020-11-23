295750bcd931cd74c5b12275c5dfc0eb0fc57c10
Devin Jameson <devinjameson@me.com>
2020-11-23 12:28:16 -0500

    Add spec for useActivationNavigation

---
+++ src/Activation/useActivationNavigation.spec.ts
@@ -0,0 +1,51 @@
+import {
+  determineActivationSteps,
+  Environment,
+} from "./useActivationNavigation"
+
+describe("useActivationNavigation", () => {
+  describe("when location is required and off, bluetooth is off, display accept terms of service is true, and enable product analytics are turned on", () => {
+    it("returns the correct set of activation steps", () => {
+      const environment: Environment = {
+        locationPermissions: "RequiredOff",
+        isBluetoothOn: false,
+        displayAcceptTermsOfService: true,
+        enableProductAnalytics: true,
+      }
+
+      const activationSteps = determineActivationSteps(environment)
+
+      const expectedActivationSteps = [
+        "AcceptTermsOfService",
+        "AnonymizedDataConsent",
+        "ActivateLocation",
+        "ActivateBluetooth",
+        "ActivateExposureNotifications",
+        "NotificationPermissions",
+        "ActivationSummary",
+      ]
+
+      expect(activationSteps).toEqual(expectedActivationSteps)
+    })
+  })
+  describe("when location is not required, bluetooth is on, display accept terms of service is false, and enable product analytics are turned off", () => {
+    it("returns the correct set of activation steps", () => {
+      const environment: Environment = {
+        locationPermissions: "NotRequired",
+        isBluetoothOn: true,
+        displayAcceptTermsOfService: false,
+        enableProductAnalytics: false,
+      }
+
+      const activationSteps = determineActivationSteps(environment)
+
+      const expectedActivationSteps = [
+        "ActivateExposureNotifications",
+        "NotificationPermissions",
+        "ActivationSummary",
+      ]
+
+      expect(activationSteps).toEqual(expectedActivationSteps)
+    })
+  })
+})
