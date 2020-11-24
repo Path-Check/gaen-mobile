2694b912e9631e23fbf11552df9a1695a3eab56b
Devin Jameson <devinjameson@me.com>
2020-11-24 10:57:44 -0500

    Update android

--- android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/reactmodules/ExposureNotificationsModule.java
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/reactmodules/ExposureNotificationsModule.java
@@ -51,7 +51,7 @@ public class ExposureNotificationsModule extends ReactContextBaseJavaModule {
     FutureCallback<Void> callback = new FutureCallback<Void>() {
       @Override
       public void onSuccess(Void result) {
-        promise.resolve(enabled ? CallbackMessages.EN_STATUS_ACTIVE : CallbackMessages.EN_STATUS_DISABLED);
+        promise.resolve(CallbackMessages.EN_STATUS_ACTIVE);
       }
 
       @Override
