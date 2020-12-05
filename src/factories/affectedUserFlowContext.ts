94c07634f6edf9b23e2b52c2c7fbf6b35b539f68
John Schoeman <johnschoeman1617@gmail.com>
2020-12-04 17:58:57 -0800

    wip

---
+++ src/factories/affectedUserFlowContext.ts
@@ -0,0 +1,14 @@
+import { Factory } from "fishery"
+
+import { AffectedUserContextState } from "../AffectedUserFlow/AffectedUserContext"
+
+export default Factory.define<AffectedUserContextState>(() => ({
+  hmacKey: "hmacKey",
+  certificate: "certificate",
+  exposureKeys: [],
+  setExposureKeys: () => {},
+  setExposureSubmissionCredentials: () => {},
+  navigateOutOfStack: () => {},
+  linkCode: undefined,
+  setLinkCode: () => {},
+}))
