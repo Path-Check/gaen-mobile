d10fcba969042986146fe1a7a5f669e661e45f75
Matt Buckley <matt@nicethings.io>
2020-12-14 13:25:43 -0600

    work in progress.

--- src/ExposureHistory/ExposureDetail.spec.tsx
+++ src/ExposureHistory/ExposureDetail.spec.tsx
@@ -3,7 +3,6 @@ import { Linking } from "react-native"
 import { render, fireEvent } from "@testing-library/react-native"
 import { useRoute, useNavigation } from "@react-navigation/native"
 
-import { DateTimeUtils } from "../utils"
 import { factories } from "../factories"
 import ExposureDetail from "./ExposureDetail"
 import { ConfigurationContext } from "../ConfigurationContext"
@@ -13,47 +12,15 @@ jest.mock("@react-navigation/native")
 describe("ExposureDetail", () => {
   describe("when the exposure happened in the last three days", () => {
     it("formats the timeframe correctly", () => {
-      const today = DateTimeUtils.beginningOfDay(Date.now())
-
       const exposureDatum = factories.exposureDatum.build({
-        date: today,
+        date: 1607973708673,
       })
       ;(useRoute as jest.Mock).mockReturnValue({
         params: { exposureDatum },
       })
 
       const { getByText } = render(<ExposureDetail />)
-      expect(getByText("Within the last 3 days")).toBeDefined()
-    })
-  })
-
-  describe("when the exposure happened four to six days ago", () => {
-    it("formats the timeframe correctly ", () => {
-      const fourDaysAgo = DateTimeUtils.beginningOfDay(DateTimeUtils.daysAgo(4))
-      const exposureDatum = factories.exposureDatum.build({
-        date: fourDaysAgo,
-      })
-
-      ;(useRoute as jest.Mock).mockReturnValue({ params: { exposureDatum } })
-
-      const { getByText } = render(<ExposureDetail />)
-      expect(getByText("4 to 6 days ago")).toBeDefined()
-    })
-  })
-
-  describe("when the exposure happened seven to fourteen days ago", () => {
-    it("formats the timeframe correctly", () => {
-      const sevenDaysAgo = DateTimeUtils.beginningOfDay(
-        DateTimeUtils.daysAgo(7),
-      )
-      const exposureDatum = factories.exposureDatum.build({
-        date: sevenDaysAgo,
-      })
-
-      ;(useRoute as jest.Mock).mockReturnValue({ params: { exposureDatum } })
-
-      const { getByText } = render(<ExposureDetail />)
-      expect(getByText("7 to 14 days ago")).toBeDefined()
+      expect(getByText("Sunday, Dec 13th - Tuesday, Dec 15th")).toBeDefined()
     })
   })
 
--- src/ExposureHistory/History/ExposureList.spec.tsx
+++ src/ExposureHistory/History/ExposureList.spec.tsx
@@ -38,10 +38,7 @@ describe("ExposureList", () => {
       )
 
       expect(queryByText("No Exposure Reports")).toBeNull()
-      expect(getAllByText("Possible COVID-19 exposure").length).toEqual(3)
-      expect(getByText("Within the last 3 days")).toBeDefined()
-      expect(getByText("4 to 6 days ago")).toBeDefined()
-      expect(getByText("7 to 14 days ago")).toBeDefined()
+      expect(getAllByText("Possible Exposure").length).toEqual(3)
     })
   })
 })
