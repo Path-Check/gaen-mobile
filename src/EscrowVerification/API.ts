7e6b138e793f06c63802309916212fe6b498991f
Matt Buckley <matt@nicethings.io>
2020-12-30 15:16:51 -0600

    Work in progress.

--- src/EscrowVerification/API.ts
+++ src/EscrowVerification/API.ts
@@ -27,7 +27,7 @@ export const submitPhoneNumber = async (
     await escrowVerificationKeySubmissionModule.submitPhoneNumber(phoneNumber)
     return { kind: "success" }
   } catch (e) {
-    Logger.error(`failed to submit phone number: `, e)
+    Logger.error(`failed to submit phone number: `, e.toString())
     switch (e.code) {
       case "403":
         return { kind: "failure", error: "RateLimit" }
@@ -60,7 +60,7 @@ export const submitDiagnosisKeys = async (
     )
     return { kind: "success" }
   } catch (e) {
-    Logger.error(`failed to submit verification code: `, e)
+    Logger.error(`failed to submit verification code: `, e.toString())
     switch (e.mssage) {
       default:
         return { kind: "failure", error: "Unknown" }
