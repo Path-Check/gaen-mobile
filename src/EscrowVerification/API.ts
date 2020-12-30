2c4f091ec38ae2fa06cc70a72bcb72b760121c57
Matt Buckley <matt.buckley@rocketinsights.com>
2020-12-30 16:36:58 -0600

    Update Escrow Verification Server Logging #806

--- src/EscrowVerification/API.ts
+++ src/EscrowVerification/API.ts
@@ -27,7 +27,7 @@ export const submitPhoneNumber = async (
     await escrowVerificationKeySubmissionModule.submitPhoneNumber(phoneNumber)
     return { kind: "success" }
   } catch (e) {
-    Logger.error(`failed to submit phone number: `, e)
+    Logger.error(`failed to submit phone number: `, e.toString())
     switch (e.code) {
       case "403":
         return { kind: "failure", error: "RateLimit" }
@@ -60,10 +60,7 @@ export const submitDiagnosisKeys = async (
     )
     return { kind: "success" }
   } catch (e) {
-    Logger.error(`failed to submit verification code: `, e)
-    switch (e.mssage) {
-      default:
-        return { kind: "failure", error: "Unknown" }
-    }
+    Logger.error(`failed to submit verification code: `, e.toString())
+    return { kind: "failure", error: "Unknown" }
   }
 }
