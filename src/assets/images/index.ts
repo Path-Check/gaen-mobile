a8674629ef1b671419863ed3eab6a74db7f9b081
johnschoeman <johnschoeman1617@gmail.com>
2021-01-07 13:15:21 -0800

    Update icon used for the share link (#815)
    Why:
    Currently the icon used for the share link is named based off the
    content. We would like for this image to be configurable, so the name
    should be changed to be based on the usage rather than the content.
    
    This commit:
    Replaces `HugEmoji.png` with `ShareImage.png`, which is the standard
    material UI 'share' graphic. We also remove some unused iconography
    styles.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -6,13 +6,13 @@ import HowItWorksIntroduction from "./howItWorksIntroduction.png"
 import HowItWorksPersonalPrivacy from "./howItWorksPersonalPrivacy.png"
 import HowItWorksPhoneRemembersDevice from "./howItWorksPhoneRemembersDevice.png"
 import HowItWorksValueProposition from "./howItWorksValueProposition.png"
-import HugEmoji from "./hugEmoji.png"
 import ProtectPrivacyExchangeKeys from "./protectPrivacyExchangeKeys.png"
 import ProtectPrivacyReceiveKeys from "./protectPrivacyReceiveKeys.png"
 import ProtectPrivacyReceiveNotification from "./protectPrivacyReceiveNotification.png"
 import ProtectPrivacySubmitKeys from "./protectPrivacySubmitKeys.png"
 import ProtectPrivacyTop from "./protectPrivacyTop.png"
 import SelfAssessment from "./selfAssessment.png"
+import ShareImage from "./shareImage.png"
 import SickEmoji from "./sickEmoji.png"
 import SmileEmoji from "./smileEmoji.png"
 import WelcomeImage from "./welcomeImage.png"
@@ -26,13 +26,13 @@ export const Images = {
   HowItWorksPersonalPrivacy,
   HowItWorksPhoneRemembersDevice,
   HowItWorksValueProposition,
-  HugEmoji,
   ProtectPrivacyExchangeKeys,
   ProtectPrivacyReceiveKeys,
   ProtectPrivacyReceiveNotification,
   ProtectPrivacySubmitKeys,
   ProtectPrivacyTop,
   SelfAssessment,
+  ShareImage,
   SickEmoji,
   SmileEmoji,
   WelcomeImage,
