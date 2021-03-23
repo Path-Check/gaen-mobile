4402637a0eccfdab3ebb4ff8c77dbd4c6ec15603
Matt Buckley <matt@smalldevshop.co>
2021-03-23 14:09:09 -0500

    work in progress.

--- src/Settings/DeleteConfirmation.tsx
+++ src/Settings/DeleteConfirmation.tsx
@@ -126,7 +126,7 @@ const DeleteConfirmation: FunctionComponent = () => {
         <TouchableOpacity
           onPress={handleOnPressDeleteAllData}
           accessibilityLabel={t("settings.delete_my_data")}
-          accessibilityHint={t("acessibility.hint.deletes_data")}
+          accessibilityHint={t("accessibility.hint.deletes_data")}
           accessibilityRole="button"
           style={style.buttonContainer}
         >
