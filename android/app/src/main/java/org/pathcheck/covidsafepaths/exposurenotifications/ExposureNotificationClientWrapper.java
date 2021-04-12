ddc3b840ad208edf5793d25e9f7f46daa979dae3
Kyle Wolff <kyle.a.wolff87@gmail.com>
2021-04-12 10:48:11 -0400

    GAEN-780 Don't Share Keys Error
    Add the start of custom exceptions for better readability and handling of custom error codes.
    Add the ability to check if an exception is type ExposureException for later use across the app

--- android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/ExposureNotificationClientWrapper.java
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/ExposureNotificationClientWrapper.java
@@ -23,6 +23,7 @@ import java.util.concurrent.TimeUnit;
 import org.pathcheck.covidsafepaths.bridge.EventSender;
 import org.pathcheck.covidsafepaths.exposurenotifications.common.AppExecutors;
 import org.pathcheck.covidsafepaths.exposurenotifications.common.TaskToFutureAdapter;
+import org.pathcheck.covidsafepaths.exposurenotifications.exceptions.ShareExposureKeysException;
 import org.pathcheck.covidsafepaths.exposurenotifications.nearby.ExposureConfigurations;
 import org.pathcheck.covidsafepaths.exposurenotifications.nearby.ProvideDiagnosisKeysWorker;
 import org.pathcheck.covidsafepaths.exposurenotifications.utils.RequestCodes;
@@ -152,7 +153,7 @@ public class ExposureNotificationClientWrapper {
         requestPermissionToGetExposureKeys(context);
       } else {
         if (getKeysCompleter != null) {
-          getKeysCompleter.setException(new Exception("Cancelled by user"));
+          getKeysCompleter.setException(new ShareExposureKeysException());
           getKeysCompleter = null;
         }
       }
