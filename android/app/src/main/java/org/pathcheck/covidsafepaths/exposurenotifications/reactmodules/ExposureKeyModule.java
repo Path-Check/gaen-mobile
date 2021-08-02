21ee43fab5f011563ffc413df504bc5ac7ca3a47
Kyle Wolff <kyle.a.wolff87@gmail.com>
2021-08-02 16:03:19 -0400

    Work/gaen 790 chaff requests (#918)
    * GAEN-790 Chaff Requests
    
    Schedule the work on Application launch
    Add the correct EventSender function to trigger the ts code
    Remove reusable code for encoding to a separate object file
    Add the new worker that schedules the periodic work
    
    * GAEN-790 Chaff Requests
    
    Add a ChaffManager class to handle specific timing for firing requests.
    
    * GAEN-790 Chaff
    
    Remove the random z variable
    
    * GAEN-790 Chaff Requests
    
    Attach a react context initializer to trigger Chaff
    Add a Config class for the ChaffManager to handle some QA needs
    
    * Added react debug menu for Android only chaff fast request call.
    
    * Added Android check to debug menu.
    
    Co-authored-by: lundjrl <jrlbidamin@gmail.com>

--- android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/reactmodules/ExposureKeyModule.java
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/exposurenotifications/reactmodules/ExposureKeyModule.java
@@ -1,12 +1,12 @@
 package org.pathcheck.covidsafepaths.exposurenotifications.reactmodules;
 
+import android.content.Context;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 import com.google.android.gms.nearby.exposurenotification.TemporaryExposureKey;
-import com.google.common.collect.ImmutableList;
 import com.google.common.io.BaseEncoding;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
@@ -16,17 +16,18 @@ import java.util.List;
 import javax.annotation.Nonnull;
 import org.jetbrains.annotations.NotNull;
 import org.pathcheck.covidsafepaths.exposurenotifications.ExposureNotificationClientWrapper;
+import org.pathcheck.covidsafepaths.exposurenotifications.chaff.ChaffManager;
 import org.pathcheck.covidsafepaths.exposurenotifications.common.AppExecutors;
 import org.pathcheck.covidsafepaths.exposurenotifications.dto.RNExposureKey;
 import org.pathcheck.covidsafepaths.exposurenotifications.network.DiagnosisKey;
 import org.pathcheck.covidsafepaths.exposurenotifications.storage.RealmSecureStorageBte;
 import org.pathcheck.covidsafepaths.exposurenotifications.utils.Util;
+import org.pathcheck.covidsafepaths.helpers.DiagnosisKeyEncoding;
 
 @SuppressWarnings("unused")
 @ReactModule(name = ExposureKeyModule.MODULE_NAME)
 public class ExposureKeyModule extends ReactContextBaseJavaModule {
   public static final String MODULE_NAME = "ExposureKeyModule";
-  private static final BaseEncoding BASE64 = BaseEncoding.base64();
 
   public ExposureKeyModule(ReactApplicationContext context) {
     super(context);
@@ -47,20 +48,8 @@ public class ExposureKeyModule extends ReactContextBaseJavaModule {
     FutureCallback<List<TemporaryExposureKey>> callback = new FutureCallback<List<TemporaryExposureKey>>() {
       @Override
       public void onSuccess(List<TemporaryExposureKey> result) {
-        final List<RNExposureKey> exposureKeys = new ArrayList<>();
-
-        List<DiagnosisKey> diagnosisKeys = toDiagnosisKeysWithTransmissionRisk(result);
-        for (DiagnosisKey k : diagnosisKeys) {
-          final String key = BASE64.encode(k.getKeyBytes());
-
-          exposureKeys.add(new RNExposureKey(
-              key,
-              k.getRollingPeriod(),
-              k.getIntervalNumber(),
-              k.getTransmissionRisk()
-          ));
-        }
-
+        List<RNExposureKey> exposureKeys =
+            DiagnosisKeyEncoding.INSTANCE.encodeDiagnosisKeys(result, false);
         promise.resolve(Util.convertListToWritableArray(exposureKeys));
       }
 
@@ -72,27 +61,6 @@ public class ExposureKeyModule extends ReactContextBaseJavaModule {
     Futures.addCallback(future, callback, AppExecutors.getLightweightExecutor());
   }
 
-  /**
-   * Transforms from EN API's TEK object to our network package's expression of it, applying a
-   * default transmission risk. This default TR is temporary, while we determine that part of the EN
-   * API's contract.
-   */
-  private List<DiagnosisKey> toDiagnosisKeysWithTransmissionRisk(
-      List<TemporaryExposureKey> recentKeys) {
-    ImmutableList.Builder<DiagnosisKey> builder = new ImmutableList.Builder<>();
-    for (TemporaryExposureKey k : recentKeys) {
-      builder.add(
-          DiagnosisKey.newBuilder()
-              .setKeyBytes(k.getKeyData())
-              .setIntervalNumber(k.getRollingStartIntervalNumber())
-              .setRollingPeriod(k.getRollingPeriod())
-              // Accepting the default transmission risk for now, which the DiagnosisKey.Builder
-              // comes with pre-set.
-              .build());
-    }
-    return builder.build();
-  }
-
   @ReactMethod
   public void storeRevisionToken(String revisionToken, final Promise promise) {
     RealmSecureStorageBte.INSTANCE.upsertRevisionToken(revisionToken);
@@ -102,5 +70,12 @@ public class ExposureKeyModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void getRevisionToken(final Promise promise) {
     promise.resolve(RealmSecureStorageBte.INSTANCE.getRevisionToken());
+  }
+
+  @ReactMethod
+  public void fetchChaffKeys(final Promise promise) {
+    Context context = getReactApplicationContext().getApplicationContext();
+    ChaffManager chaffManager = ChaffManager.getInstance(context);
+    promise.resolve(chaffManager.getChaffKeys());
   }
 }
---
+++ android/app/src/test/java/org/pathcheck/covidsafepaths/exposurenotifications/chaff/ChaffManagerTest.kt
@@ -0,0 +1,160 @@
+package org.pathcheck.covidsafepaths.exposurenotifications.chaff
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.SystemClock
+import android.util.Log
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.slot
+import io.mockk.unmockkStatic
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeTrue
+import org.amshove.kluent.shouldHaveSize
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.pathcheck.covidsafepaths.exposurenotifications.dto.RNExposureKey
+import org.pathcheck.covidsafepaths.exposurenotifications.utils.TimeProvider
+import java.security.SecureRandom
+import java.util.concurrent.TimeUnit
+
+internal class ChaffManagerTest {
+
+    private val requestFiredSlot = slot<Long>()
+    private val chaffJsonSlot = slot<String>()
+
+    private val sharedPreferences: SharedPreferences = mockk {
+        every {
+            edit().putLong("requestFired", capture(requestFiredSlot)).apply()
+        }.answers {
+            // no-op
+        }
+
+        every {
+            edit().putString("chaffJson", capture(chaffJsonSlot)).apply()
+        } answers {
+            // no-op
+        }
+
+        every {
+            getString("chaffJson", "")
+        }.answers {
+            chaffJsonSlot.captured
+        }
+
+        every {
+            getLong("requestFired", 0L)
+        }.answers {
+            requestFiredSlot.captured
+        }
+    }
+    private val context: Context = mockk {
+        every {
+            getSharedPreferences("ChaffManagerSharedPref", Context.MODE_PRIVATE)
+        }.returns(sharedPreferences)
+    }
+
+    private var mockDouble = 0.0
+    private val secureRandomMock: SecureRandom = mockk {
+        every {
+            nextDouble()
+        }.answers {
+            mockDouble
+        }
+    }
+
+    private var mockTime = 1627009456526L
+    private val timeProviderMock: TimeProvider = mockk {
+        every {
+            currentTimeInMillis
+        }.answers {
+            mockTime
+        }
+    }
+
+    private lateinit var chaffManager: ChaffManager
+
+    @BeforeEach
+    fun setUp() {
+        chaffManager = ChaffManager.createChaffManager(context, timeProviderMock, secureRandomMock)
+
+        mockkStatic(Log::class)
+        mockkStatic(SystemClock::class)
+
+        every {
+            SystemClock.uptimeMillis()
+        }.returns(0L)
+
+        every {
+            Log.d(any(), any())
+        }.returns(0)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkStatic(Log::class)
+        unmockkStatic(SystemClock::class)
+
+        requestFiredSlot.clear()
+        chaffJsonSlot.clear()
+    }
+
+    @Test
+    fun `Given we save the keys, when 24 hours has passed and the probability is true, then we should fire`() {
+        val rnExposureKeys = listOf(
+            RNExposureKey("This is the key", 1, 2, 3)
+        )
+
+        mockDouble = .04
+
+        chaffManager.save(rnExposureKeys)
+
+        mockTime += TimeUnit.HOURS.toMillis(24)
+
+        chaffManager.shouldFire().shouldBeTrue()
+    }
+
+    @Test
+    fun `Given we save the keys, when 24 hours has not passed and the probability is true, then we should not fire`() {
+        val rnExposureKeys = listOf(
+            RNExposureKey("This is the key", 1, 2, 3)
+        )
+
+        mockDouble  = .05
+
+        chaffManager.save(rnExposureKeys)
+
+        mockTime += TimeUnit.HOURS.toMillis(20)
+
+        chaffManager.shouldFire().shouldBeFalse()
+    }
+
+    @Test
+    fun `Given we save the keys, when 24 hours has passed and the probability is false, then we should not fire`() {
+        val rnExposureKeys = listOf(
+            RNExposureKey("This is the key", 1, 2, 3)
+        )
+
+        mockDouble = .09
+
+        chaffManager.save(rnExposureKeys)
+
+        mockTime += TimeUnit.HOURS.toMillis(24)
+
+        chaffManager.shouldFire().shouldBeFalse()
+    }
+
+    @Test
+    fun `Given we have a list of RNExposureKeys, when we save, then get the saved keys they are parsed correctly`() {
+        val rnExposureKeys = listOf(
+            RNExposureKey("This is the key", 1, 2, 3)
+        )
+        chaffManager.save(rnExposureKeys)
+        chaffManager.getSavedRNExposureKeys().run {
+            this!!.shouldHaveSize(1)
+            this[0].key = "This is the key"
+        }
+    }
+}
